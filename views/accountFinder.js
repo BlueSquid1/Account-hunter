8f882d9193f0a28156514e75882be42bdee448b8
blueSquid1 <clinton.page123@gmail.com>
2017-12-12 22:18:46 +1030

    updated .ignore file to included front end javascript files

---
+++ views/accountFinder.js
@@ -0,0 +1,30 @@
+$('#loading-image').show();
+
+$.ajax({
+    url: '../savingAccounts',
+    success: function (accountsArray) {
+        //for each bank account
+        for (var accountIndex in accountsArray) {
+            var accountObj = accountsArray[accountIndex];
+
+            var accountRow = $('<tr></tr>');
+
+            accountRow.append($('<th></th>').addClass('text-center').text(Number(accountIndex) + 1));
+            accountRow.append($('<td></td>').addClass('text-center').text(accountObj["accountName"]));
+            accountRow.append($('<td></td>').addClass('text-center').text(accountObj["interestedEarned"]));
+            accountRow.append($('<td></td>').addClass('text-center').text(accountObj["maxVarInterestRate"]));
+            accountRow.append($('<td></td>').addClass('text-center').text(accountObj["standardInterestRate"]));
+            accountRow.append($('<td></td>').addClass('text-center').text(accountObj["monthlyFees"]));
+            accountRow.append($('<td></td>').addClass('text-center').text(accountObj["minOpeningDeposit"]));
+
+            var urlLink = $('<a></a>').attr('href', accountObj["moreDetailsurl"]);
+            urlLink.addClass('text-center').text("details");
+            accountRow.append($('<td></td>').append(urlLink));
+
+            $("table#savingAccountsTb tbody").append(accountRow);
+        }
+    },
+    complete: function () {
+        $('#loading-image').hide();
+    }
+});
